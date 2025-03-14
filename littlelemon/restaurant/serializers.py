2a77180bcaf16404b9daba47bf3815c6aa5cad11
Siddharth Kumar <ybsid@ybsid-macbook.local>
2025-03-14 20:06:56 +0530

    menu-item API

---
+++ littlelemon/restaurant/serializers.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User 
+from rest_framework import serializers
+from .models import Menu
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User 
+        fields = ['url','username','email','groups']
+
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = "__all__"
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView , RetrieveUpdateAPIView , DestroyAPIView
+from .models import Menu
+from .serializers import MenuItemSerializer
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(RetrieveUpdateAPIView , DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
 
 # Create your views here.
 def index(request):
